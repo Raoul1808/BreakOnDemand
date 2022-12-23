3b489711bd653b19d7c57b5b7295b47d861f749c
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-12-23 12:29:04 +0100

    Initial Commit

---
+++ BreakOnDemand/BreakOnDemand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OWML.Common;
+using OWML.ModHelper;
+
+namespace BreakOnDemand;
+
+public class BreakOnDemand : ModBehaviour
+{
+    private void Awake()
+    {
+        // You won't be able to access OWML's mod helper in Awake.
+        // So you probably don't want to do anything here.
+        // Use Start() instead.
+    }
+
+    private void Start()
+    {
+        // // Starting here, you'll have access to OWML's mod helper.
+        // ModHelper.Console.WriteLine($"My mod {nameof(BreakOnDemand)} is loaded!", MessageType.Success);
+        //
+        // // Example of accessing game code.
+        // LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+        // {
+        //     if (loadScene != OWScene.SolarSystem) return;
+        //     ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        // };
+    }
+
+    private void Update()
+    {
+        if (OWInput.IsPressed(InputLibrary.rollMode) && OWInput.IsPressed(InputLibrary.cancel) && Locator.GetProbe().IsLaunched())
+        {
+            ModHelper.Console.WriteLine("oiaaiioaioai PARADOX INTERACTIVE HAS LAUNCHED OAAIIAORAIEIAZOEIUA", MessageType.Success);
+            Locator.GetTimelineObliterationController().BeginTimelineObliteration(TimelineObliterationController.ObliterationType.TIME_LOOP_EXPERIMENT, Locator.GetProbe());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Break on Demand
+
+Meet your terrible fate on demand with the simple press of 2 buttons!
+  
+Steps to trigger it:
+1. Launch your probe
+2. Press both the Back/Cancel button and the Roll Mode button
+3. You did it!
